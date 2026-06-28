a5188e8198b1aa6e6348ab801b493c06ca51a6cb
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-06-29 00:18:14 +0530

    feat: add zod validation and enhance graph AI service with solution and judge nodes

--- Projects/Battle_AI_Arena/src/app.ts
+++ Projects/Battle_AI_Arena/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import useGraph from './services/graph.ai.service.js';
 
 const app = express();
 
@@ -6,6 +7,10 @@ app.get('/health', (_, res) => {
   return res.status(200).json({
     status: true,
   });
+});
+
+app.post('/use-graph', async (req, res) => {
+  await useGraph('What is the capital of France?');
 });
 
 export default app;
