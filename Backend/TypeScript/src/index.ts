5c3dc0b3ba1317b171a4196a86e8439fe52e93f5
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-06-28 09:36:21 +0530

    feat: initialize TypeScript project with essential configuration files and structure

---
+++ Backend/TypeScript/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * string , number , boolean , array, object , tuple ,void, never
+ * type
+ */
+
+/**
+ * any , Unknown
+ */
+
+let a: unknown;
+
+a = 'hello';
+
+if (typeof a === 'string') console.log(a.toUpperCase());
