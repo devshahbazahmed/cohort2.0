61ffc9bdd3db112c46a5d386c957c821a1442ccf
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-07-05 12:43:13 +0530

    feat: enhance Card component to support like and dislike functionality

--- ReactRefresher/Basics-of-React/src/App.jsx
+++ ReactRefresher/Basics-of-React/src/App.jsx
@@ -2,26 +2,36 @@ import { useState } from 'react';
 import Card from './components/Card';
 
 const App = () => {
-  const [count, setCount] = useState(0);
   const [postData, setPostData] = useState([
     {
+      id: 1,
       name: 'Ritik',
       role: 'SDE',
       likeCount: 0,
     },
-    {
-      name: 'Shiv',
-      role: 'Forward Engg',
-      likeCount: 20,
-    },
+    { id: 2, name: 'Shiv', role: 'Forward Engg', likeCount: 20 },
   ]);
 
-  const like = () => {
-    setCount(count + 1);
+  const like = (id) => {
+    setPostData((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, likeCount: item.likeCount + 1 } : item
+      )
+    );
+  };
+
+  const dislike = (id) => {
+    setPostData((prev) =>
+      prev.map((item) =>
+        item.id === id && item.likeCount > 0
+          ? { ...item, likeCount: item.likeCount - 1 }
+          : item
+      )
+    );
   };
   return (
     <div className="px-10 py-5">
-      <Card postData={postData} like={like} count={count} />
+      <Card postData={postData} like={like} dislike={dislike} />
     </div>
   );
 };
--- ReactRefresher/Basics-of-React/src/components/Card.jsx
+++ ReactRefresher/Basics-of-React/src/components/Card.jsx
@@ -1,12 +1,11 @@
-const Card = ({ postData, like, count }) => {
-  console.log(postData);
+const Card = ({ postData, like, dislike }) => {
   return (
     <div className="w-full flex gap-10">
       {postData.map((post) => {
         return (
           <div
             className="card flex flex-col justify-center items-center w-70 h-70 bg-zinc-600 rounded-md text-center"
-            key={post.name}
+            key={post.id}
           >
             <img
               src="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -15,15 +14,18 @@ const Card = ({ postData, like, count }) => {
             />
             <h1>{post.name}</h1>
             <p>{post.role}</p>
-            <p>Like Count: {count}</p>
+            <p>Like Count: {post.likeCount}</p>
             <div className="w-full flex gap-3 items-center justify-center mt-2">
               <button
-                onClick={like}
+                onClick={() => like(post.id)}
                 className="px-8 py-3 bg-blue-600 rounded-md hover:cursor-pointer active:scale-105"
               >
                 Like
               </button>
-              <button className="px-8 py-3 bg-red-600 rounded-md hover:cursor-pointer active:scale-105">
+              <button
+                onClick={() => dislike(post.id)}
+                className="px-8 py-3 bg-red-600 rounded-md hover:cursor-pointer active:scale-105"
+              >
                 Dislike
               </button>
             </div>
