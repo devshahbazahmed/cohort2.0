547e20b02c5f8fc94073ed46a588ae703fb8cb3e
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-07-11 22:55:06 +0530

    feat: update color scheme and styles for TopNav and global CSS variables

--- Projects/slatedesk-cms/src/features/dashboard/ui/components/TopNav.jsx
+++ Projects/slatedesk-cms/src/features/dashboard/ui/components/TopNav.jsx
@@ -5,7 +5,7 @@ import { Bell } from 'lucide-react';
 const TopNav = () => {
   return (
     <div className="flex justify-between items-center">
-      <div className="flex gap-4 items-center bg-(--secondary) w-[30%] rounded-lg px-3 py-2 border-gray-600">
+      <div className="flex gap-4 items-center bg-(--bg-main) text-(--text-primary) w-[30%] rounded-lg px-3 py-2 border-gray-600">
         <Search size={24} />
         <input
           type="search"
