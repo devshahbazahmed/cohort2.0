a838afa6ee4ba1ffc6a8658848fb722031f8f789
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-07-18 21:54:34 +0530

    feat: refactor main.js to improve responsiveness and animation logic

--- Animations/04-Threejs/src/main.js
+++ Animations/04-Threejs/src/main.js
@@ -2,15 +2,22 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 // Scene
 
 const scene = new THREE.Scene();
+
+const clock = new THREE.Clock();
 
 // Camera
 
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  size.width / size.height,
   0.01,
   100
 );
@@ -26,6 +33,12 @@ const material = new THREE.MeshBasicMaterial({
 });
 
 const cube = new THREE.Mesh(geometry, material);
+
+// cube.position.set(-1.5, -2, -1.4);
+cube.position.x = -1.5;
+cube.scale.x = 2;
+
+cube.rotation.x = Math.PI / 10;
 
 scene.add(cube);
 
@@ -46,10 +59,21 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 controls.enableDamping = true;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(size.width, size.height);
+
+window.addEventListener('resize', () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(size.width, size.height);
+});
 
 const animate = () => {
-  cube.rotation.y += 0.09;
+  const delta = clock.getElapsedTime();
+  cube.rotation.y = delta;
 
   renderer.render(scene, camera);
 
