55e7072979973a1401c9ef7fc0ff190be0bd9191
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-07-22 17:49:17 +0530

    Add initial project setup with videos, styles, and React components

---
+++ Projects/awwwards/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl font-bold font-circular-web text-violet-300">
+        Welcome to Awwwards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
