1101f62f17eceb64eea3cb08417f6f24d8cffaa0
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-07-31 18:23:11 +0530

    feat: add Contact, Features, Story, Footer, and RoundedCorners components; update App component to include new sections and enable autoplay for Hero video

--- Projects/awwwards/src/App.jsx
+++ Projects/awwwards/src/App.jsx
@@ -1,6 +1,10 @@
 import About from './components/About';
+import Contact from './components/Contact';
+import Features from './components/Features';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Story from './components/Story';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -8,6 +12,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
